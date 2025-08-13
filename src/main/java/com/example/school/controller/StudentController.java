7e18c548c5fb731abb8f47741f9ce4247c85156b
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-08-13 17:36:42 +0000

    Implemented getStudent Controller using @GetMapping annotation

--- src/main/java/com/example/school/controller/StudentController.java
+++ src/main/java/com/example/school/controller/StudentController.java
@@ -6,3 +6,27 @@
  * import java.util.*;
  *
  */
+package com.example.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.school.model.Student;
+import com.example.school.service.StudentH2Service;
+
+@RestController
+public class StudentController {
+
+    private final StudentH2Service service;
+
+    @Autowired
+    public StudentController(StudentH2Service service) {
+        this.service = service;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return service.getStudents();
+    }
+}
